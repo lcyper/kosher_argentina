8a3e89505eb4c0838e28a084decc7ae01a0d1090
lcyper <lcyper@gmail.com>
2021-10-21 23:44:04 -0300

    add shared preferences

--- lib/services/categories_service.dart
+++ lib/services/categories_service.dart
@@ -4,16 +4,33 @@ import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:kosher_ar/models/category.dart';
 import 'package:kosher_ar/models/product.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CategoriesService {
   final Dio _dio = Dio();
 
   getData() async {
     try {
-      List _categoriesList =
-          await _getDataFromUrl('https://www.kosher.org.ar/api/categorias.php');
-      List _productsList =
-          await _getDataFromUrl('https://www.kosher.org.ar/api/products.php');
+      List _categoriesList;
+      List _productsList;
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? categories = prefs.getString('categoriesList');
+      String? products = prefs.getString('productsList');
+      if (categories != null && products != null) {
+        debugPrint('Trayendo datos desde shared preferences');
+        _categoriesList = jsonDecode(categories);
+        _productsList = jsonDecode(products);
+      } else {
+        _categoriesList = await _getDataFromUrl(
+            'https://www.kosher.org.ar/api/categorias.php');
+        _productsList =
+            await _getDataFromUrl('https://www.kosher.org.ar/api/products.php');
+
+        prefs.setString('categoriesList', jsonEncode(_categoriesList)); //await
+        prefs.setString('productsList', jsonEncode(_productsList)); //await
+        debugPrint('Guardando datos en shared preferences');
+      }
 
       List<Category> _categories = [];
       for (Map<String, dynamic> map in _categoriesList) {
@@ -26,7 +43,6 @@ class CategoriesService {
       }
 
       return {'categories': _categories, 'products': _products};
-
     } catch (e) {
       debugPrint('Error: ' + e.toString());
       return e.toString();
