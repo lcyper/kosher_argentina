f505cda76a6757c0416130ccd2e5e0473907dc51
lcyper <lcyper@gmail.com>
2021-11-07 23:16:46 -0300

    CategoriesService try load from internet first

--- lib/services/categories_service.dart
+++ lib/services/categories_service.dart
@@ -9,28 +9,20 @@ import 'package:shared_preferences/shared_preferences.dart';
 class CategoriesService {
   final Dio _dio = Dio();
 
-  getData() async {
+  Future<Map> getData() async {
+    List _categoriesList;
+    List _productsList;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
     try {
-      List _categoriesList;
-      List _productsList;
+      _categoriesList =
+          await _getDataFromUrl('https://www.kosher.org.ar/api/categorias.php');
+      _productsList =
+          await _getDataFromUrl('https://www.kosher.org.ar/api/products.php');
 
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      String? categories = prefs.getString('categoriesList');
-      String? products = prefs.getString('productsList');
-      if (categories != null && products != null) {
-        debugPrint('Trayendo datos desde shared preferences');
-        _categoriesList = jsonDecode(categories);
-        _productsList = jsonDecode(products);
-      } else {
-        _categoriesList = await _getDataFromUrl(
-            'https://www.kosher.org.ar/api/categorias.php');
-        _productsList =
-            await _getDataFromUrl('https://www.kosher.org.ar/api/products.php');
-
-        prefs.setString('categoriesList', jsonEncode(_categoriesList)); //await
-        prefs.setString('productsList', jsonEncode(_productsList)); //await
-        debugPrint('Guardando datos en shared preferences');
-      }
+      prefs.setString('categoriesList', jsonEncode(_categoriesList)); //await
+      prefs.setString('productsList', jsonEncode(_productsList)); //await
+      debugPrint('Guardando datos en shared preferences');
 
       List<Category> _categories = [];
       for (Map<String, dynamic> map in _categoriesList) {
@@ -42,10 +34,35 @@ class CategoriesService {
         _products.add(Product.fromMap(map));
       }
 
-      return {'categories': _categories, 'products': _products};
+      return {'categories': _categories, 'products': _products, 'local': false};
     } catch (e) {
+      String? categories = prefs.getString('categoriesList');
+      String? products = prefs.getString('productsList');
+
+      if (categories != null && products != null) {
+        debugPrint('Trayendo datos desde shared preferences');
+        _categoriesList = jsonDecode(categories);
+        _productsList = jsonDecode(products);
+
+        List<Category> _categories = [];
+        for (Map<String, dynamic> map in _categoriesList) {
+          _categories.add(Category.fromMap(map));
+        }
+
+        List<Product> _products = [];
+        for (Map<String, dynamic> map in _productsList) {
+          _products.add(Product.fromMap(map));
+        }
+
+        return {
+          'categories': _categories,
+          'products': _products,
+          'local': true
+        };
+      }
+
       debugPrint('Error: ' + e.toString());
-      return e.toString();
+      return {'Error': e.toString()};
     }
   }
 
