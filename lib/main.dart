f22758ec401d01db3dd043a41a71d46cc10c00ee
lcyper <lcyper@gmail.com>
2024-08-05 17:30:16 -0300

    put themeData in separated file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kosher_ar/screens/home_screen.dart';
+import 'package:kosher_ar/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+  
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Kosher Argentina',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
+      theme: themeData,
       routes: {
         '/': (context) =>  HomeScreen(),
       },
---
+++ lib/theme.dart
@@ -0,0 +1,5 @@
+  import 'package:flutter/material.dart';
+
+ThemeData themeData = ThemeData(
+        primarySwatch: Colors.orange,
+      );
