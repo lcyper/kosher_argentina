6426dc8ec81a795f867e2133706f1bd76636415e
lcyper <lcyper@gmail.com>
2025-03-24 18:52:21 -0300

    drawer menu no usado

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+Drawer appDrawer() {
+  // TODO: value, onTopNavigatorPageName
+  List<String> menuOptions = [
+    'Supervisaciones',
+    'Donaciones',
+    'Configuracion',
+    'Repostar Bug',
+    'Contacto'
+  ];
+  return Drawer(
+    // backgroundColor: Colors.orange,
+    child: ListView(
+      children: [
+        const DrawerHeader(
+          child: Center(
+            child: Text(
+              'Menu',
+              style: TextStyle(fontSize: 32),
+            ),
+          ),
+        ),
+        ...menuOptions.map(
+          (option) => ListTile(
+            title: Text(option),
+            onTap: () {},
+          ),
+        )
+      ],
+    ),
+  );
+}
